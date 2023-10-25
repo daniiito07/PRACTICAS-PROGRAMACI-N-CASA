bd6b7205c4fc552ce5a8a9f6e1a026a0b5f5d07f
daniiito07 <d3anielj117@gmail.com>
2023-10-24 23:58:14 -0600

    p015 terminada
    Daniel Reyes, actualizada

--- P015_Batallapokemon_V0/P015_Batallapokemon_V0/P015_Batallapokemon_V0.cpp
+++ P015_Batallapokemon_V0/P015_Batallapokemon_V0/P015_Batallapokemon_V0.cpp
@@ -4,37 +4,66 @@
 #include <iostream>
 #include <string>
 #include <ctime>
+#include <cstdlib>
+#include <locale.h>
 
 int main() {
+    setlocale(LC_ALL, "ES_MX.UTF-8");
     srand(time(0));
-
-    int saludgyarados = 100;
-    int saludmewtwo = 100;
 
     std::string Gyarados = "Gyarados";
     std::string Mewtwo = "Mewtwo";
 
-    while (saludgyarados > 0 && saludmewtwo > 0) 
-    {
-        int ataquegyarados = rand() % 25 + 1;
-        std::cout << Gyarados << " ataca a " << Mewtwo << " por " << ataquegyarados << " puntos de daño" << std::endl;
-        saludmewtwo -= ataquegyarados;
+    bool continuar = true;
 
-        if (saludmewtwo <= 0) 
-        {
-            std::cout << Mewtwo << " ha sido derrotado. " << Gyarados << " ha ganado" << std::endl;
-            break;
+    while (continuar) {
+        int saludgyarados = 100;
+        int saludmewtwo = 100;
+
+        while (saludgyarados > 0 && saludmewtwo > 0) {
+            std::cout << "Es tu turno. Elige una opción:\n";
+            std::cout << "1. Atacar\n";
+            std::cout << "2. Esquivar\n";
+            std::cout << "3. Huir\n";
+
+            int opcion;
+            std::cin >> opcion;
+
+            int ataquegyarados = 0;
+
+            switch (opcion) {
+            case 1: 
+                ataquegyarados = rand() % 25 + 1;
+                std::cout << "Atacas a " << Mewtwo << " por " << ataquegyarados << " puntos de daño." << std::endl;
+                saludmewtwo -= ataquegyarados;
+                break;
+            case 2: 
+                std::cout << "Intentas esquivar el ataque de " << Mewtwo << "." << std::endl;
+                break;
+            case 3: 
+                std::cout << "Has decidido huir de la batalla. " << Mewtwo << " gana." << std::endl;
+                return 0; 
+            default:
+                std::cout << "Opción inválida. Elige una opción válida." << std::endl;
+                continue; 
+            }
+
+            if (saludmewtwo <= 0) {
+                std::cout << Mewtwo << " ha sido derrotado. " << Gyarados << " gana!!" << std::endl;
+                break;
+            }
+            int ataquemewtwo = rand() % 20 + 1;
+            std::cout << Mewtwo << " ataca a " << Gyarados << " por " << ataquemewtwo << " puntos de daño." << std::endl;
+            saludgyarados -= ataquemewtwo;
+
+            if (saludgyarados <= 0) {
+                std::cout << Gyarados << " ha sido derrotado. " << Mewtwo << " gana!!" << std::endl;
+                break;
+            }
         }
 
-        int ataquemewtwo = rand() % 20 + 1;
-        std::cout << Mewtwo << " ataca a " << Gyarados << " por " << ataquemewtwo << " puntos de daño" << std::endl;
-        saludgyarados -= ataquemewtwo;
-
-        if (saludgyarados <= 0) 
-        {
-            std::cout << Gyarados << " ha sido derrotado. " << Mewtwo << " ha ganado" << std::endl;
-            break;
-        }
+        std::cout << "¿Quieres pelear de nuevo? (1 para sí, 0 para no): ";
+        std::cin >> continuar;
     }
 
     return 0;
